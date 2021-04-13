1ce147257ada2030eb543aa23dfea3231ace6c91
smartfridge <37928912+smartfrigde@users.noreply.github.com>
2021-04-13 19:24:45 +0200

    Update and rename LinuxED.py to LinuxSC.py

--- LinuxSC.py
+++ LinuxSC.py
@@ -2,7 +2,7 @@
 
 # -*- coding: utf-8 -*-
 
-# written by Creatable
+# written by Creatable/ modified by fridge
 
 # credits:
 # - Tcll5850: https://gitlab.com/Tcll
@@ -24,19 +24,19 @@ if __name__ == "__main__":
                             'If you are unaware of what you\'re doing, it\'s recommended you close this installer.\n'
                             'Otherwise you may continue at your own risk.\n')"""
 
-    if os.name == 'nt': print('WARNING: it appears you are running LinuxED on Windows.\n'
-                            'LinuxED was not originally made for Windows and Windows compatibility is not maintained.\n'
+    if os.name == 'nt': print('WARNING: it appears you are running LinuxSC on Windows.\n'
+                            'LinuxSC was not originally made for Windows and Windows compatibility is not maintained.\n'
                             'Continue at your own risk.')
     # Define the starting variables, these are all their own thing.
     username = getpass.getuser()
     dirpath = os.path.realpath('')
     currentdir = True
     if 'XDG_DATA_HOME' in os.environ:
-        if os.path.exists(os.path.join(os.environ['XDG_DATA_HOME'], "EnhancedDiscord")):
+        if os.path.exists(os.path.join(os.environ['XDG_DATA_HOME'], "SmartCord")):
             dirpath = os.environ['XDG_DATA_HOME']
             currentdir = False
     elif 'HOME' in os.environ:
-         if os.path.exists(os.path.join(os.environ['HOME'], '.local', 'share', "EnhancedDiscord")):
+         if os.path.exists(os.path.join(os.environ['HOME'], '.local', 'share', "SmartCord")):
             dirpath = os.path.join(os.environ['HOME'], '.local', 'share')
             currentdir = False
     filepath = os.path.dirname(os.path.realpath(__file__))
@@ -46,7 +46,7 @@ if __name__ == "__main__":
     detect_versions = lambda discordpath,idxsubpath: [
         (discordpath+vsn+idxsubpath, vsn) for vsn in (os.listdir(discordpath) if os.path.exists(discordpath) else []) if os.path.isdir(discordpath+vsn) and len(vsn.split('.')) == 3 ]
 
-    print('Welcome to the LinuxED installation script.')
+    print('Welcome to the LinuxSC installation script.')
 
     baseclients = {
         "STABLE" : detect_versions('/home/%s/.config/discord/'%username, '/modules/discord_desktop_core/index.js'),
@@ -119,7 +119,7 @@ if __name__ == "__main__":
         while True:
             print('\nOperating on client: %s %s\n'%(client,version))
             print('Please type the number for your desired option:')
-            enhanceddir = os.path.join(dirpath, "EnhancedDiscord")
+            enhanceddir = os.path.join(dirpath, "SmartCord")
             if os.name == 'nt':
                 injdir = 'process.env.injDir = "%s"' % enhanceddir.encode('unicode_escape').decode("utf-8")
             else:
@@ -131,7 +131,7 @@ if __name__ == "__main__":
             module.exports = require('./core.asar');"""%injdir
 
             # room for expansion (other params can be provided here)
-            optionsdict = [('Install ED',),('Uninstall ED',),('Update ED',),('Update LinuxED',),('Select Client',)]
+            optionsdict = [('Install SC',),('Uninstall SC',),('Update SC',),('Update LinuxSC',),('Select Client',)]
             if currentdir == False:
                 optionsdict.append(('Use current directory',))
             else:
@@ -152,46 +152,45 @@ if __name__ == "__main__":
                 break # shouldn't get here, but just in case.
             
             
-            elif option == 'Update LinuxED':
-                print("Updating LinuxED...")
-                urllib.request.urlretrieve('https://github.com/Cr3atable/LinuxED/archive/master.zip', '%s/LinuxEDUpdate.zip' % tempdir)
-                with zipfile.ZipFile("%s/LinuxEDUpdate.zip" % tempdir,"r") as zip_ref:
+            elif option == 'Update LinuxSC':
+                print("Updating LinuxSC...")
+                urllib.request.urlretrieve('https://github.com/smartfrigde/LinuxSC/archive/master.zip', '%s/LinuxSCUpdate.zip' % tempdir)
+                with zipfile.ZipFile("%s/LinuxSCUpdate.zip" % tempdir,"r") as zip_ref:
                     zip_ref.extractall(tempdir)
-                os.remove("%s/LinuxED-master/LICENSE" % tempdir)
-                os.remove("%s/LinuxED-master/README.md" % tempdir)
-                os.remove("%s/LinuxED-master/.gitignore" % tempdir)
-                os.remove("%s/LinuxED-master/plugins/style.css" % tempdir)
-                shutil.move("%s/LinuxED-master/LinuxED.py" % tempdir, "%s/LinuxED-master/%s" % (tempdir, scriptname))
-                distutils.dir_util.copy_tree('%s/LinuxED-master/' % tempdir, filepath)
-                shutil.rmtree("%s/LinuxED-master" % tempdir)
-                os.remove("%s/LinuxEDUpdate.zip" % tempdir)
+                os.remove("%s/LinuxSC-master/LICENSE" % tempdir)
+                os.remove("%s/LinuxSC-master/README.md" % tempdir)
+                os.remove("%s/LinuxSC-master/.gitignore" % tempdir)
+                shutil.move("%s/LinuxSC-master/LinuxSC.py" % tempdir, "%s/LinuxSC-master/%s" % (tempdir, scriptname))
+                distutils.dir_util.copy_tree('%s/LinuxSC-master/' % tempdir, filepath)
+                shutil.rmtree("%s/LinuxSC-master" % tempdir)
+                os.remove("%s/LinuxSCUpdate.zip" % tempdir)
                 if os.name != "nt":
                     try:
                         os.system("chmod +x %s" % os.path.join(filepath, scriptname))
                     except:
                         print("Couldn't make script executable...\nYou may experience problems when trying to use it again")
-                print("Update complete!\nPlease restart LinuxED for the update to take effect.")
+                print("Update complete!\nPlease restart LinuxSC for the update to take effect.")
         
         
-            elif option == 'Uninstall ED':
-                print('Uninstalling EnhancedDiscord...')
+            elif option == 'Uninstall SC':
+                print('Uninstalling SmartCord...')
                 if os.path.exists(backuppath):
                     print("Making sure index.js is no longer read only before removing it...")
                     os.chmod(jspath, S_IWUSR|S_IREAD)
                     os.remove(jspath)
                     shutil.move(backuppath, jspath)
-                    print("Successfully uninstalled EnhancedDiscord!")
+                    print("Successfully uninstalled SmartCord!")
                 else:
                     print("Error: Couldn't find index.js backup, did you use the installer to install ED?\n")
         
-            elif option == 'Install ED':
+            elif option == 'Install SC':
                 if not os.path.exists(enhanceddir):
-                    print("Downloading ED...")
-                    urllib.request.urlretrieve('https://github.com/joe27g/EnhancedDiscord/archive/master.zip', '%s/EnhancedDiscord.zip' % tempdir)
-                    with zipfile.ZipFile("%s/EnhancedDiscord.zip" % tempdir,"r") as zip_ref:
+                    print("Downloading SC...")
+                    urllib.request.urlretrieve('https://github.com/smartfrigde/smartcord/archive/main.zip', '%s/SmartCord.zip' % tempdir)
+                    with zipfile.ZipFile("%s/SmartCord.zip" % tempdir,"r") as zip_ref:
                         zip_ref.extractall(tempdir)
-                    shutil.move("%s/EnhancedDiscord-master" % tempdir, "%s/EnhancedDiscord" % dirpath)
-                    os.remove("%s/EnhancedDiscord.zip" % tempdir)
+                    shutil.move("%s/smartcord-main" % tempdir, "%s/SmartCord" % dirpath)
+                    os.remove("%s/SmartCord.zip" % tempdir)
                 
                 if not os.path.exists(backuppath):
                     print("Creating index.js.backup...")
@@ -211,18 +210,18 @@ if __name__ == "__main__":
             
                 print("EnhancedDiscord installation complete!\n")
 
-            elif option == 'Update ED':
+            elif option == 'Update SC':
                 if os.path.exists(enhanceddir):
-                    print("Updating ED...")
-                    urllib.request.urlretrieve('https://github.com/joe27g/EnhancedDiscord/archive/master.zip', '%s/EDUpdate.zip' % tempdir)
-                    with zipfile.ZipFile("%s/EDUpdate.zip" % tempdir,"r") as zip_ref:
+                    print("Updating SC...")
+                    urllib.request.urlretrieve('https://github.com/smartfrigde/smartcord/archive/main.zip', '%s/SCUpdate.zip' % tempdir)
+                    with zipfile.ZipFile("%s/SCUpdate.zip" % tempdir,"r") as zip_ref:
                         zip_ref.extractall(tempdir)
-                    distutils.dir_util.copy_tree('%s/EnhancedDiscord-master' % tempdir, '%s/EnhancedDiscord' % dirpath)
-                    shutil.rmtree("%s/EnhancedDiscord-master" % tempdir)
-                    os.remove("%s/EDUpdate.zip" % tempdir)
+                    distutils.dir_util.copy_tree('%s/smartcord-main' % tempdir, '%s/SmartCord' % dirpath)
+                    shutil.rmtree("%s/smartcord-main" % tempdir)
+                    os.remove("%s/SCUpdate.zip" % tempdir)
                     print("Update complete!")
                 else:
-                    print("It seems EnhancedDiscord is not installed in the current directory so it was unable to be updated.")
+                    print("It seems SmartCord is not installed in the current directory so it was unable to be updated.")
             elif option == 'Select Client':
                 print("Selecting new Discord client...")
                 backup = (client, jspath, version)
